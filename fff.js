aca65c9b37172dcfc94ba84f9da0446849d4ede2
zakaria1qk <zakariarommane296@gmail.com>
2024-12-29 21:16:16 +0100

    Create fff.js
    gggg

---
+++ fff.js
@@ -0,0 +1,25 @@
+const hamburger  = document.querySelector('.humburger');
+
+const  navMenu = document.querySelector('.nav-menu');
+
+
+
+hamburger.addEventListener("click", ()=>{
+
+    hamburger.classList.toggle("active");
+
+    navMenu.classList.toggle("active");
+
+})
+
+
+
+
+
+document.querySelectorAll(".nav-link").forEach(n  => n.addEventListener("click", ()=>{
+
+    hamburger.classList.remove("active");
+
+    navMenu.classList.remove("active");
+
+}))
